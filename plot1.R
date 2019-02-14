5b227ecc3f0ec5bb85e076933b88b85ce16adf47
vidushi97 <anandvidushi2@gmail.com>
2019-02-14 21:54:48 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+t <- read.table("C:/Users/Abc/Desktop/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+t <- t[complete.cases(t),]
+dateTime <- paste(t$Date, t$Time)
+dateTime <- setNames(dateTime, "DateTime")
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+t <- cbind(dateTime, t)
+t$dateTime <- as.POSIXct(dateTime)
+hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
